daca221891c7eca62dc44dcadf0234642af9cab2
Muhammad Asharul <masharul51@gmail.com>
2024-08-27 20:31:50 +0700

    add: handiling request

---
+++ src/app/posts/data.ts
@@ -0,0 +1,20 @@
+export const post = [
+  {
+    id: 1,
+    text: "this is post 1",
+    userId: 3,
+    description: "lorem ipsum",
+  },
+  {
+    id: 2,
+    text: "this is post 2",
+    userId: 2,
+    description: "lorem ipsum",
+  },
+  {
+    id: 3,
+    text: "this is post 3",
+    userId: 5,
+    description: "lorem ipsum",
+  },
+];
---
+++ src/app/posts/route.ts
@@ -0,0 +1,4 @@
+import { post } from "./data";
+export async function GET() {
+  return Response.json(post);
+}
